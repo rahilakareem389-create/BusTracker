66254c31e7c41701037674ef398c9b26fdd69422
Rahila Kareem <rahilakareem389@gmail.com>
2026-09-14 11:43:09 +0500

    Fix CORS policy to allow Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.use(cors({ 
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174'], 
+  origin: function (origin, callback) {
+    callback(null, true); // Allow all origins dynamically
+  },
   credentials: true 
 }));
 
